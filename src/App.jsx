1ea52091ccad2c6a83f429c683a381b4142d2096
AngelPASTORROJAS <angeldaniel.pastorrojas@gmail.com>
2023-06-01 09:47:13 +0200

    feat: ajout de comment récuperer la variable du contexte

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
+import { useContext } from 'react'
 import './App.css'
+import { Context } from './reduces/provider'
 
 function App() {
+  const [state, dispatch] = useContext(Context)
   return (
     <>
+    {state.count}
     </>
   )
 }
